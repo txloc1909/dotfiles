7a61423097e61709f7e6862c8764f16f50bb58c1
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2021-07-13 00:27:38 +0700

    silly whitespaces fix

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # make CaspLock an additional Escape
-setxkbmap -option caps:escape
+setxkbmap -option caps:escape &
 
 # network manager applet 
-nm-applet &
+nm-applet & 
 
 # load configs from ~/.Xresources
-xrdb -merge ~/.Xresources
+xrdb -merge ~/.Xresources &
 
 # set wallpaper
 nitrogen --restore &
--- vimrc
+++ vimrc
@@ -94,7 +94,7 @@ if 'VIRTUAL_ENV' in os.environ:
     env_base_dir = os.environ['VIRTUAL_ENV']
     activate_this = os.path.join(env_base_dir, 'bin/activate_this.py')
     exec(open(activate_this).read(), dict(__file__=activate_this))
-EOF 
+EOF
 
 
 
