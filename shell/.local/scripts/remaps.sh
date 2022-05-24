cf41aad11f02519aff3058138fc3ca8fa94a2ceb
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2022-05-24 13:22:29 +0700

    minor changes

--- shell/.local/scripts/remaps.sh
+++ shell/.local/scripts/remaps.sh
@@ -11,7 +11,7 @@ setxkbmap -option caps:super
 killall xcape 2>/dev/null; xcape -e 'Super_L=Escape'
 
 # Map the menu button to right super as well
-xmodmap -e 'keycode 135 = Super_R'
+xmodmap -e 'keycode 135 = Super_L'
 
 # Turn off capslock if on, since there is no longer a key for it
 xset -q | grep "Caps Lock:\s on" && xdotool key Caps_Lock
