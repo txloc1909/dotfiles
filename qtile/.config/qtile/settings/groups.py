5f64ecbaf3148ea39085801e5b9258503a6f162a
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2022-09-04 12:58:02 +0700

    update window rules

--- qtile/.config/qtile/settings/groups.py
+++ qtile/.config/qtile/settings/groups.py
@@ -9,7 +9,18 @@ _group_names = list("123456789")
 
 _group_rules = {
     "1": [Match(wm_class="firefox")],
+    "2": [
+        Match(wm_class="local-tmux"),
+        Match(wm_class="Gnome-terminal"),
+    ],
     "3": [Match(wm_class="notion-snap", wm_instance_class="notion-snap")],
+    "4": [Match(wm_class="obsidian", wm_instance_class="obsidian")],
+    "6": [Match(wm_class="Virt-manager", wm_instance_class="virt-manager")],
+    "7": [
+        Match(wm_class="Skype", wm_instance_class="skype"),
+        Match(wm_class="discord", wm_instance_class="discord"),
+        Match(wm_class="Slack", wm_instance_class="slack"),
+    ],
     "8": [
         Match(wm_class="Spotify", wm_instance_class="spotify"),
         Match(wm_class="Brave-browser", wm_instance_class="youtube.com"),
@@ -21,9 +32,15 @@ _group_rules = {
 }
 
 _default_layout = {
-    "1": "max",
+    "1": "monadtall",
+    "2": "max",
+    "3": "monadtall",
+    "4": "max",
+    "5": "monadtall",
+    "6": "max",
+    "7": "max",
     "8": "max",
-    "9": "max",
+    "9": "monadtall",
 }
 
 
@@ -50,4 +67,5 @@ def init_group_keys():
         )
 
     keys.append(Key([mod], "0", swap_groups_between_two_screen))
+    keys.append(Key([alt, "control"], "0", swap_groups_between_two_screen))
     return keys
