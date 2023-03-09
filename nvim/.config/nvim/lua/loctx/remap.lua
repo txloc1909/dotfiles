6f1fb3fb63fbe27059307154c63f44ddc79ed3a1
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-03-09 23:56:05 +0700

    [neovim] more homemade keybinds

--- nvim/.config/nvim/lua/loctx/remap.lua
+++ nvim/.config/nvim/lua/loctx/remap.lua
@@ -1,6 +1,10 @@
 vim.g.mapleader = " "
+
+-- netrw
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- toggle recent buffers
+vim.keymap.set("n", "<leader><Tab>", "<C-^>")
 
 -- moving lines in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -9,6 +13,8 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- cursor stays in middle when haft-page scrolling 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "{", "{zz")
+vim.keymap.set("n", "}", "}zz")
 
 -- cursor stays in middle when searching
 vim.keymap.set("n", "n", "nzzzv")
