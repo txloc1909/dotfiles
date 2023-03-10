9d6ad942bb959119d00b89ce82bcaf9f5c45bff6
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-03-11 01:05:23 +0700

    [neovim] hightlighting for vim-signify

---
+++ nvim/.config/nvim/after/plugin/vim-signify.lua
@@ -0,0 +1,5 @@
+vim.cmd [[
+    highlight SignifySignAdd    ctermfg=green  guifg=#00ff00 cterm=NONE gui=NONE
+    highlight SignifySignDelete ctermfg=red    guifg=#ff0000 cterm=NONE gui=NONE
+    highlight SignifySignChange ctermfg=yellow guifg=#ffff00 cterm=NONE gui=NONE
+]]
