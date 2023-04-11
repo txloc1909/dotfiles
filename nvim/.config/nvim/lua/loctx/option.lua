6fde911136fe49ae5e6115e71c25553eb3c93848
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-04-11 15:26:03 +0700

    [neovim] modify indents and tabs

--- nvim/.config/nvim/lua/loctx/option.lua
+++ nvim/.config/nvim/lua/loctx/option.lua
@@ -8,10 +8,16 @@ vim.opt.scrolloff = 9999
 vim.opt.signcolumn = "yes"
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-vim.opt.smartindent = true
+vim.opt.autoindent = true
 vim.opt.colorcolumn = "80"
 vim.opt.showmatch = true
 vim.opt.hidden = true
 vim.opt.confirm = true
 vim.opt.lazyredraw = true
 vim.opt.wrap = false
+
+-- recommendation from https://www.reddit.com/r/vim/wiki/tabstop/
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
