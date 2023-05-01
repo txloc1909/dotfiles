1a2184afdf635a2eee62e09deaf267920b7d2b93
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-05-02 01:08:05 +0700

    [awesome] first tyrannical set up

--- awesome/.config/awesome/README.md
+++ awesome/.config/awesome/README.md
@@ -8,23 +8,14 @@
 - Floating is secondary, but still needed
 
 ## Dependencies
-On Ubuntu/Debian, install `awesome` and `awesome-extra`
-`feh` for setting wallpaper
+- `awesome-git`
+- `feh`
+- `volumeicon`
+- `picom`
+- `nm-applet`
 
 ### Lua packages
 - `tyrannical`: for tag management
 
 ## Materials
 - To break down default `rc.lua` to different modules: [article](https://epsi-rns.github.io/desktop/2019/06/15/awesome-overview.html)
-
-TODO
-[ ] Design tags: names, keys, rules
-[ ] Multi-monitor workflow
-[x] Set window title bar when floating, and turn off when tiling
-    [ ] Currently, switching to floating layout does not toggle titlebar
-        for all windows affected by the floating layout
-[x] Assign normal executable name for flatpak apps
-[x] Find out why new `$PATH`s are not appended
-	- Need `~/.profile`
-[x] How to autostart programs (background processes/daemons) at logins
-[x] How to autostart ibus-daemon
--- awesome/.config/awesome/main/tags.lua
+++ awesome/.config/awesome/main/tags.lua
@@ -1,5 +1,6 @@
 local awful = require("awful")
 local gears = require("gears")
+local tyrannical = require("tyrannical")
 
 local tile = awful.layout.suit.tile
 local max = awful.layout.suit.max
@@ -15,59 +16,98 @@ local default_tag_opt = {
 
 local my_tags = {
 	{
-		name = "Work",
-		key = "u",
-		opt = { }
+		name            = "Work",
+		key             = "u",
+                init            = true,
+                exclusive       = false,
+                layout          = awful.layout.suit.tile,
+                exec_once       = "org.chromium.Chromium",
+                selected        = false,
+                class           = { "Chromium-browser" },
+                instance        = { "notion.so", "slack", "kitty-workterm"},
 	},
 	{
-		name = "Term",
-		key = "i",
-		opt = { }
+		name            = "Term",
+		key             = "i",
+                init            = true,
+                exclusive       = true,
+                layout          = awful.layout.suit.tile,
+                exec_once       = "kitty",
+                selected        = false,
+                class           = { "kitty", "Gnome-terminal", "kgx", "alacritty" },
 	},
 	{
-		name = "Personal",
-		key = "o",
-		opt = { }
+		name            = "Personal",
+		key             = "o",
+                init            = true,
+                exclusive       = true,
+                exec_once       = "com.brave.Browser",
+                layout          = awful.layout.suit.tile,
+                selected        = false,
+                instance        = { "brave-browser" },
 	},
 	{
-		name = "Browse",
-		key = "w",
-		opt = {
-			layout = tile,
-		}
+		name            = "Browse",
+		key             = "w",
+                init            = true,
+                exclusive       = true,
+                layout          = awful.layout.suit.tile,
+                selected        = false,
+                instance        = { "facebook.com", "reddit.com", "brave-browser"},
+                class           = { "firefox", "qutebrowser", "Vivaldi-stable" },
 	},
 	{
-		name = "Chat",
-		key = "e",
-		opt = {
-			layout = tile,
-			master_width_factor = 0.6,
-		}
+		name            = "Chat",
+		key             = "e",
+                init            = true,
+                exclusive       = true,
+                exec_once       = "org.ferdium.Ferdium",
+                layout          = awful.layout.suit.tile,
+                selected        = false,
+                class           = { "discord", "Caprine", "Skype", "Ferdium" },
 	},
 	{
-		name = "Vid",
-		key = "y",
-		opt = {
-			layout = max
-		}
+		name            = "Vid",
+		key             = "y",
+                init            = true,
+                exclusive       = true,
+                exec_once       = "flatpak run com.brave.Browser --app=https://youtube.com",
+                layout          = awful.layout.suit.max,
+                selected        = false,
+                class           = { "mpv", "Totem" },
+                instance        = { "youtube.com" },
 	},
 	{
-		name = "Misc",
-		key = "m",
-		opt = {
-			layout = max
-		}
+		name            = "Misc",
+		key             = "m",
+                fallback        = true,
+                selected        = false,
+                layout          = awful.layout.suit.max,
 	}
 }
 
+-- tyrannical.settings: keys
+-- block_children_focus_stealing
+-- force_odd_as_intrusive
+-- favor_focused
+-- tag
+    -- tag.layout
+    -- tag.master_width_factor
+-- default_layout
+-- master_width_factor
+
+
 function _M.get ()
-	awful.screen.connect_for_each_screen(function(s)
-		for _, my_tag in ipairs(my_tags) do
-			awful.tag.add(my_tag.name,
-				gears.table.join(default_tag_opt, my_tag.opt, { screen = s })
-			)
-		end
-	end)
+        tyrannical.settings.default_layout = awful.layout.suit.tile
+        tyrannical.settings.master_width_factor = 0.55
+        tyrannical.tags = my_tags
+
+        tyrannical.properties.floating = {
+            "gnome-calculator",
+        }
+        tyrannical.properties.centered = {
+            "gnome-calculator",
+        }
 
 	return my_tags
 end
