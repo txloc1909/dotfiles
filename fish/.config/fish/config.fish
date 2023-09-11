3753ec9d811fc7998988fcd722afad95f386b0d3
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-09-11 20:57:05 +0700

    [misc] replace exa (unmaintained) by eza

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,17 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    if type -q eza
+        alias ls='eza --icons --group-directories-first'
+        alias ll='eza -lahF --icons --group-directories-first'
+        alias la='eza -1a --icons --group-directories-first'
+    else
+        alias ls='ls --color=auto --group-directories-first'
+        alias ll='ls -alFh'
+        alias la='ls -A'
+    end
+
+
     setenv PYENV_ROOT "$HOME/.pyenv"
     setenv PATH "$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"
     pyenv rehash
