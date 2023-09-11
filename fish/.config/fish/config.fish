4793261f6557e0d6a0b5c2eea7842b824237495e
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-09-11 20:53:13 +0700

    [fish] set up `pyenv` in fish shell

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    setenv PYENV_ROOT "$HOME/.pyenv"
+    setenv PATH "$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"
+    pyenv rehash
+    pyenv init - | source
 end
