3f88598bdc830a13b44ba77aca1bd5898374b555
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-11-02 22:56:18 +0700

    split LSP configs

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -1,4 +1,4 @@
 -- Toggle fugitive window
-vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = 'Toogle [G]it [S]tatus' })
 
 -- vim: ts=2 sts=2 sw=2 et
