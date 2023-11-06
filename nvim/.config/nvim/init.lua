4d1ebf261454c01a34b45a9faa55a2eec26dd540
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-11-06 23:31:33 +0700

    Split highlight.on_yank to its own plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -18,18 +18,6 @@ if not vim.loop.fs_stat(lazypath) then
   }
 end
 vim.opt.rtp:prepend(lazypath)
-
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
-
 require('lazy').setup("plugins")
 
 -- vim: ts=2 sts=2 sw=2 et
